709cd8e9eac050337444e3240ab670393ecb8375
kristopher <kristopher@acm.org>
2023-06-14 13:02:31 -0500

    streamline bin script :ski:

--- bin/scrape_raw_charges.py
+++ bin/scrape_raw_charges.py
@@ -1,35 +1,14 @@
-import googlesearch
-import time
+from func import facility
 
-data_file = 'raw_il'  # your input file here 
+def main():
+    # Get all facilities
+    all_facilities = facility.get_all_facilities()
 
-with open(data_file, "r") as f:
-    for line in f.read().splitlines():
-        ldomains = []
-        lurls = []
-        lprices = []
-        lsheets = []
-        try:
-          for j in googlesearch.search(line, lang='en', num=10, stop=10, pause=20):
-            ldomain = j.split("//")[-1].split("/")[0].split('?')[0]
-            if ldomain in ['www.ahd.com', 'healthcare4ppl.com']:
-                continue
+    # Iterate over all facilities
+    for fac in all_facilities:
+        # Scrape each facility
+        facility.scrape_facility(fac)
 
-            lurls.append(j)
-            ldomains.append(ldomain)
-            for k in googlesearch.search("site:%s price transparency" % ldomain, lang='en', num=10, stop=10, pause=20):
-                if '.pdf' in k or '.doc' in k:
-                    continue
-                lprices.append(k)
-                # Note, this code was lost in 2021 and needs to be recreated.  
-                #  TODO: pull down the URL. Look for any tables, test to see if there are charge codes
-                #  if so, parse each row into the following format
-                #  Facility ID     Facility Name   Date    Source Sheet    Bill ID Description     Charge
-                # ex
-                #  140174  PRESENCE MERCY MEDICAL CENTER   12/15/2019      https://docs.google.com/spreadsheets/d/1YbLeMdXSDyM_WxLa3WmKc72opaF6ZH3Ott4OVZYuEjs/edit#gid=608674038  61010095        MRI UPR EXTR RT W/O CON $3,237.00
-                # sort sheet is not that import and just for your own reference.  
-
-        except Exception as e:
-          print("exception:", e)
-          pass
+if __name__ == "__main__":
+    main()
 
